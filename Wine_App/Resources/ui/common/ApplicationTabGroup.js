034b5e1e814723afc5bfb4d32fffb357d7ce6b74
Eric Wooley <5wooley4@gmail.com>
2013-01-24 09:14:40 -0800

    Changed Tabbed Names

--- Wine_App/Resources/ui/common/ApplicationTabGroup.js
+++ Wine_App/Resources/ui/common/ApplicationTabGroup.js
@@ -3,25 +3,32 @@ function ApplicationTabGroup(Window) {
 	var self = Ti.UI.createTabGroup();
 	
 	//create app tabs
-	var win1 = new Window(L('home')),
-		win2 = new Window(L('settings'));
+	var win1 = new Window(L('Profile')),
+		win2 = new Window(L('Wineries')),
+		win3 = new Window(L('My Wines'));
 	
 	var tab1 = Ti.UI.createTab({
-		title: L('home'),
+		title: L('My Profile'),
 		icon: '/images/KS_nav_ui.png',
 		window: win1
 	});
 	win1.containingTab = tab1;
 	
 	var tab2 = Ti.UI.createTab({
-		title: L('settings'),
+		title: L('Wineries'),
 		icon: '/images/KS_nav_views.png',
 		window: win2
 	});
 	win2.containingTab = tab2;
 	
+	var tab3 = Ti.UI.createTab({
+		title: L('My Wines'),
+		window: win3
+	});
+	
 	self.addTab(tab1);
 	self.addTab(tab2);
+	self.addTab(tab3);
 	
 	return self;
 };
